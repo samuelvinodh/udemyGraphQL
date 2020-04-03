fc573581ba5dd23a729876308ddfc23238c9a943
Vinodh Samuel <samuelvinodh@gmail.com>
2020-04-03 16:30:15 -0400

    Basic Project Commit

---
+++ myGraphQLBasic/src/main/java/com/sam/graphql/MyGraphQlBasicApplication.java
@@ -0,0 +1,13 @@
+package com.sam.graphql;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyGraphQlBasicApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyGraphQlBasicApplication.class, args);
+	}
+
+}
